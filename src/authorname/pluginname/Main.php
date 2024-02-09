a146e2038f5ebfa7338454105ca3a26de7430469
PresentKim <debe3721@gmail.com>
2024-02-10 00:27:20 +0900

    :wrench: Update .editorconfig file

--- src/authorname/pluginname/Main.php
+++ src/authorname/pluginname/Main.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace authorname\pluginname;
@@ -6,15 +7,15 @@ namespace authorname\pluginname;
 use pocketmine\plugin\PluginBase;
 
 final class Main extends PluginBase{
-    protected function onLoad() : void{
-        $this->getLogger()->info("Template plugin loaded!");
-    }
+	protected function onLoad() : void{
+		$this->getLogger()->info("Template plugin loaded!");
+	}
 
-    protected function onEnable() : void{
-        $this->getLogger()->info("Template plugin enabled!");
-    }
+	protected function onEnable() : void{
+		$this->getLogger()->info("Template plugin enabled!");
+	}
 
-    protected function onDisable() : void{
-        $this->getLogger()->info("Template plugin disabled!");
-    }
+	protected function onDisable() : void{
+		$this->getLogger()->info("Template plugin disabled!");
+	}
 }
