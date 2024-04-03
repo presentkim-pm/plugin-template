e6e94e5ff5f0faa886b27f11c558aa7bbc3c1bcd
PresentKim <debe3721@gmail.com>
2024-04-03 12:50:52 +0900

    :art: Code formatting with .editorconfig - indent by space instead of tab

--- src/kim/present/pluginname/Main.php
+++ src/kim/present/pluginname/Main.php
@@ -30,15 +30,16 @@ namespace kim\present\pluginname;
 use pocketmine\plugin\PluginBase;
 
 final class Main extends PluginBase{
-	protected function onLoad() : void{
-		$this->getLogger()->info("Template plugin loaded!");
-	}
 
-	protected function onEnable() : void{
-		$this->getLogger()->info("Template plugin enabled!");
-	}
+    protected function onLoad() : void{
+        $this->getLogger()->info("Template plugin loaded!");
+    }
 
-	protected function onDisable() : void{
-		$this->getLogger()->info("Template plugin disabled!");
-	}
+    protected function onEnable() : void{
+        $this->getLogger()->info("Template plugin enabled!");
+    }
+
+    protected function onDisable() : void{
+        $this->getLogger()->info("Template plugin disabled!");
+    }
 }
